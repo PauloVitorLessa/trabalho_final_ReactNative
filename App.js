62d7abf15b8cb677f16ccc3ec0d4ec26b158c2b4
Rodrigo Bastos <rdgbastos@icloud.com>
2023-06-29 15:25:24 -0300

    pequenas correções

--- App.js
+++ App.js
@@ -3,17 +3,7 @@ import { DataProvider } from "./src/context/DataContext";
 import { EditoraProvider } from "./src/context/EditoraContext";
 import { LivroProvider } from "./src/context/LivroContext";
 import { PaperProvider } from "react-native-paper";
-
 import Routes from "./src/routes";
-
-import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
